ce2795206908c7cf0d5cb1baa733986338ede635
ali <mokhtar40_a@yahoo.com>
2024-06-07 14:37:24 +0330

    solve some bugs

--- returant.js
+++ returant.js
@@ -4,14 +4,14 @@ let foods = [
     { type: 'فرنگی', src: 'imges/farangi/fingerspicy.jpg', name: 'مرغ انگشتی', price: 365000, },
     { type: 'فرنگی', src: 'imges/farangi/ketfsokhari.jpg', name: 'کتف سوخاری', price: 480000, },
     { type: 'فرنگی', src: 'imges/farangi/shincel.jpg', name: 'شینسل مرغ', price: 435000, },
-    { type: 'فرنگی', src: './imges/farangi/چاینیز میکس.jpg', name: 'چاینیز میکس', price: 455000, },
-    { type: 'فرنگی', src: 'imges/farangi/پارمژان مرغ.jpg', name: 'پارمژان مرغ', price: 415000, },
-    { type: 'فرنگی', src: 'imges/farangi/چیکن رول.jpg', name: 'چیکن رول', price: 400000, },
-    { type: 'فرنگی', src: 'imges/farangi/اسکالپ مرغ.jpg', name: 'اسکالپ مرغ', price: 397000, },
+    { type: 'فرنگی', src: './imges/farangi/chines mix.jpg', name: 'چاینیز میکس', price: 455000, },
+    { type: 'فرنگی', src: './imges/farangi/chiken parmejan.jpg', name: 'پارمژان مرغ', price: 415000, },
+    { type: 'فرنگی', src: './imges/farangi/chiken roll.jpg', name: 'چیکن رول', price: 400000, },
+    { type: 'فرنگی', src: './imges/farangi/chiken skalp.jpg', name: 'اسکالپ مرغ', price: 397000, },
     { type: 'گریل', src: 'imges/grill/1gcva1hh.nz1_560x350.jpg', name: 'استیک مرغ', price: 590000, },
     { type: 'گریل', src: 'imges/grill/payar.jpg', name: 'استیک پایار', price: 750000, },
     { type: 'گریل', src: 'imges/grill/meat.jpg', name: 'استیک گوشت', price: 800000, },
-    { type: 'گریل', src: 'imges/grill/گریل میکس.jpg', name: 'گریل میکس', price: 700000, },
+    { type: 'گریل', src: './imges/grill/grill mix.jpg', name: 'گریل میکس', price: 700000, },
     { type: 'فست فود', src: './imges/pitza/makhlut pitza.jpg', name: 'پیتزا مخلوط', price: 365000, },
     { type: 'فست فود', src: 'imges/pitza/mecsikanpizza.jpg', name: 'پیتزا مخصوص', price: 360000, },
     { type: 'فست فود', src: 'imges/pitza/pitza.jpg', name: 'پیتزا استیک', price: 410000, },
@@ -24,9 +24,9 @@ let foods = [
     { type: 'نوشیدنی', src: 'imges/drinks/water.jpg', name: 'اب معدنی', price: 7000, },
     { type: 'نوشیدنی', src: 'imges/drinks/fanat.jpg', name: 'فانتا', price: 20000, },
     { type: 'نوشیدنی', src: 'imges/drinks/dogh.jpg', name: 'دوغ', price: 25000, },
-    { type: 'نوشیدنی', src: './imges/drinks/دوغ لیوانی.jpg', name: 'دوغ لیوانی', price: 40000, },
-    { type: 'نوشیدنی', src: 'imges/drinks/اب میوه طبیعی.jpg', name: 'اب میوه طبیعی', price: 80000, },
-    { type: 'نوشیدنی', src: './imges/drinks/دلستر.jpg', name: 'دلستر', price: 25000, },
+    { type: 'نوشیدنی', src: './imges/drinks/dough.jpg', name: 'دوغ لیوانی', price: 40000, },
+    { type: 'نوشیدنی', src: "./imges/drinks/fresh drinks.jpg", name: 'اب میوه طبیعی', price: 80000, },
+    { type: 'نوشیدنی', src: './imges/drinks/delester.jpg', name: 'دلستر', price: 25000, },
 ]
 
 
@@ -38,6 +38,8 @@ let tax = document.getElementById('tax')
 let total = document.getElementById('total')
 let count = document.getElementById('count')
 let choose = document.getElementById('choose')
+let zeroTax = document.getElementById('zeroTax')
+let zeroTotal = document.getElementById('zeroTotal')
 let mybasket = []
 
 
@@ -63,11 +65,11 @@ function render(list) {
     root.innerHTML = mylist.join('')
 }
 
-
 function addfood(food) {
+    zeroTax.textContent = ''
+    zeroTotal.textContent = ''
     let allfoods = foods.find(function (fod) { return fod.name == food })
     mybasket.push(allfoods)
-    console.log(mybasket)
     let aux = 0
     for (i = 0; i < mybasket.length; i++) {
         aux += mybasket[i].price
